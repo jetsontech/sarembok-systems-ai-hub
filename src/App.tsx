0504a3608a807eecac4fe84ca06e542473d07031
Sarembok Systems <deploy@sarembok.com>
2025-12-03 01:56:56 -0500

    Fix routing and desktop layout issues - WIP navigation debugging

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import ToolView from './pages/ToolView';
 import AgentPlayground from './pages/AgentPlayground';
+import Nexus365 from './components/Nexus365';
 import ContextSidebar from './components/ContextSidebar';
 import { ProjectProvider } from './context/ProjectContext';
 import './App.css';
@@ -11,14 +12,22 @@ function App() {
   return (
     <ProjectProvider>
       <Router>
-        <Layout>
-          <ContextSidebar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tool/:id" element={<ToolView />} />
-            <Route path="/playground" element={<AgentPlayground />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          {/* Nexus365 has its own layout, so it's outside the main Layout */}
+          <Route path="/nexus" element={<Nexus365 />} />
+
+          {/* All other routes use the main Layout */}
+          <Route path="/*" element={
+            <Layout>
+              <ContextSidebar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/tool/:id" element={<ToolView />} />
+                <Route path="/playground" element={<AgentPlayground />} />
+              </Routes>
+            </Layout>
+          } />
+        </Routes>
       </Router>
     </ProjectProvider>
   );
